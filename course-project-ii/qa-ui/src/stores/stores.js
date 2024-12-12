3bfb34ed20a4b1458b4b48a12a7940003cf3afce
OliverForest_ <yinanhu519@gmail.com>
2024-12-12 18:23:56 +0100

    title,README

--- course-project-ii/qa-ui/src/stores/stores.js
+++ course-project-ii/qa-ui/src/stores/stores.js
@@ -1,4 +1,4 @@
-import { readable, writable } from "svelte/store";
+import { readable} from "svelte/store";
 
 // user
 let user = localStorage.getItem("userUuid");
@@ -10,35 +10,3 @@ if (!user) {
 
 export const userUuid = readable(user);
 
-// // has posted question
-
-// let storedHasPostedQuestion = localStorage.getItem("hasPostedQuestion")
-
-// if(!storedHasPostedQuestion) {
-//   storedHasPostedQuestion = new Date();
-//   localStorage.setItem("hasPostedQuestion", storedHasPostedQuestion);
-// }
-
-// const hasPostedQuestion = writable(storedHasPostedQuestion)
-
-// hasPostedQuestion.subscribe(value => {
-//   localStorage.setItem("hasPostedQuestion", value);
-// })
-
-
-// // has posted answer
-
-// let storedHasPostedAnswer = localStorage.getItem("hasPostedAnswer")
-
-// if(!storedHasPostedAnswer) {
-//   storedHasPostedAnswer = true;
-//   localStorage.setItem("hasPostedAnswer", storedHasPostedAnswer);
-// }
-
-// const hasPostedAnswer = writable(storedHasPostedAnswer)
-
-// hasPostedAnswer.subscribe(value => {
-//   localStorage.setItem("hasPostedAnswer", value);
-// })
-
-// export {userUuid, hasPostedQuestion, hasPostedAnswer};
